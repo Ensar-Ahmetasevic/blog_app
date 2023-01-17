f739d976ed70a27d0fc18ca6dfa4a3cd7d1b1300
Ensar Ahmetasevic <111744786+Ensar-Ahmetasevic@users.noreply.github.com>
2023-01-17 21:40:28 +0100

    change the pass for DB

--- pages/api/contact.js
+++ pages/api/contact.js
@@ -28,7 +28,7 @@ async function handler(req, res) {
     let client;
 
     const connectionString =
-      "mongodb+srv://Ensar_MongoDB:Love1986+@cluster0.1odepbc.mongodb.net/my-site-dev?retryWrites=true&w=majority";
+      "mongodb+srv://Ensar_MongoDB:G6nOlo0WUDHxntqV@cluster0.1odepbc.mongodb.net/my-site-dev?retryWrites=true&w=majority";
     // const connectionString = `mongodb+srv://${process.env.mongodb_username}:${process.env.mongodb_password}@${process.env.mongodb_clustername}.1odepbc.mongodb.net/${process.env.mongodb_database}?retryWrites=true&w=majority`;
     // we use this code when is anable "module.exports" function in next.config.js
     try {
